37640d4d28a761ba1292f03765768fda647512d8
Paulishable <paulzelenski@msn.com>
2022-06-20 11:28:39 -0700

    fixed weight problem when edge doesn't exist

--- application.py
+++ application.py
@@ -1,52 +1,41 @@
 from graph import *
 
 my_graph = Graph()
-a_vertex = Vertex("A")
-b_vertex = Vertex("B")
-c_vertex = Vertex("C")
-d_vertex = Vertex("D")
-e_vertex = Vertex("E")
-f_vertex = Vertex("F")
 
-my_graph.add_vertex(a_vertex)
-my_graph.add_vertex(b_vertex)
-my_graph.add_vertex(c_vertex)
-my_graph.add_vertex(d_vertex)
-my_graph.add_vertex(e_vertex)
-my_graph.add_vertex(f_vertex)
+my_graph.add_vertex("A")
+my_graph.add_vertex("B")
+my_graph.add_vertex("C")
+my_graph.add_vertex("D")
+my_graph.add_vertex("E")
+my_graph.add_vertex("F")
 
-my_graph.add_directed_edge(a_vertex, f_vertex, 9.0)
-my_graph.add_directed_edge(a_vertex, b_vertex, 2.0)
-my_graph.add_directed_edge(b_vertex, f_vertex, 6.0)   # assume B to F only
-my_graph.add_directed_edge(b_vertex, d_vertex, 15.0)
-my_graph.add_directed_edge(b_vertex, c_vertex, 8.0)
-my_graph.add_directed_edge(f_vertex, e_vertex, 3.0)
-my_graph.add_directed_edge(c_vertex, d_vertex, 1.0)
-my_graph.add_directed_edge(e_vertex, c_vertex, 7.0)
-my_graph.add_directed_edge(e_vertex, d_vertex, 3.0)
-
-print("the weight: ", my_graph.get_weight(a_vertex, f_vertex))
+my_graph.add_directed_edge("A", "B", 2.0)
+my_graph.add_directed_edge("A", "F", 9.0)
+my_graph.add_directed_edge("B", "F", 6.0)
+my_graph.add_directed_edge("B", "D", 15.0)
+my_graph.add_directed_edge("B", "C", 8.0)
+my_graph.add_directed_edge("C", "D", 1.0)
+my_graph.add_directed_edge("E", "C", 7.0)
+my_graph.add_directed_edge("E", "D", 3.0)
+my_graph.add_directed_edge("F", "B", 3.0)
+my_graph.add_directed_edge("F", "E", 3.0)
 
 print()
+out = str(my_graph)
+print(out)
 
-
-
-
-print("starting BFS with vertex A")
-for vertex in my_graph.breadth_first_search(a_vertex):
-    print(vertex.label, end = "")
+print("Starting BFS with Vertex A")
+for vertex_label in my_graph.bfs("A"):
+    print(vertex_label, end="")
 print()
 
+print("Starting DFS with Vertex A")
+for vertex_label in my_graph.dfs("A"):
+    print(vertex_label, end="")
+print()
+
+print("get weight:", my_graph.get_weight("B", "A"))
 
 
 
 
-# A -> B[label="2.0",weight="2.0"];
-# A -> F[label="9.0",weight="9.0"];
-# B -> C[label="8.0",weight="8.0"];
-# B -> D[label="15.0",weight="15.0"];
-# B -> F[label="6.0",weight="6.0"];
-# C -> D[label="1.0",weight="1.0"];
-# E -> C[label="7.0",weight="7.0"];
-# E -> D[label="3.0",weight="3.0"];
-# F -> B[label="6.0",weight="6.0"]; F -> E[label="3.0",weight="3.0"];
--- graph.py
+++ graph.py
@@ -1,10 +1,10 @@
 import sys
-from queue import  *
+from queue import *
+import math
 
-if __name__ == '__main__':
-    if len(sys.argv) != 1:
-        Hello = str(sys.argv[1])
-        print(Hello)
+
+def main():
+    pass
 
 
 class Vertex:
@@ -20,42 +20,109 @@ class Graph:
         self.edge_weights = {}
 
     def __str__(self):
-        return self.adjacency_list[0][0]
+        string = "digraph G {\n"
+        for item in self.adjacency_list:
+            for subitem in self.adjacency_list[item]:
 
-    def add_vertex(self, new_vertex):
-        self.adjacency_list[new_vertex] = []
-        print(self.adjacency_list[new_vertex])
+                weight = self.get_weight(item.label, subitem.label)
 
-    def add_edge(self, from_vertex, to_vertex, weight=1.0):
+                string += f"{item.label}  ->  {subitem.label}[label=\"{weight}\",weight=\"{weight}\"]\n"
+
+        string += "}"
+
+        return string
+
+    def get_vertex(self, vertex_label):
+        """get the vertex in the graph that matched the label"""
+        for item in self.adjacency_list:
+            if item.label == vertex_label:
+                return item
+            for subitem in self.adjacency_list[item]:
+                if subitem.label == vertex_label:
+                    return subitem
+        return None
+
+    def edge_does_not_exist(self, from_vertex_label, to_vertex_label):
+        """get the vertex in the graph that matched the label"""
+        for item in self.adjacency_list:
+            if item.label == from_vertex_label:
+                for subitem in self.adjacency_list[item]:
+                    if subitem.label == to_vertex_label:
+                        print("here in tester-from", item.label)
+                        print("here in tester-to", subitem.label)
+                        return False
+        return True
+
+    def add_vertex(self, new_vertex_label):
+        """take a label, make it into a Vertex and save it"""
+
+        self.adjacency_list[Vertex(new_vertex_label)] = []
+        return self
+
+    def add_edge(self, from_vertex_label, to_vertex_label, weight=1.0):
         """needed for assignment - assume the edge is not directed """
-        if not isinstance(from_vertex, Vertex):
+        if not isinstance(from_vertex_label, str):
             raise ValueError("Label is not a Vertex")
-        if not isinstance(to_vertex, Vertex):
+        if not isinstance(to_vertex_label, str):
             raise ValueError("Label is not a Vertex")
         if not isinstance(weight, float):
             raise ValueError("Label is not a float")
 
-        self.add_undirected_edge(from_vertex, to_vertex, weight)
+        from_vertex = self.get_vertex(from_vertex_label)
+        to_vertex = self.get_vertex(to_vertex_label)
+        if from_vertex is None or to_vertex is None:
+            raise ValueError("Not a vertex in the graph")
 
-    def add_directed_edge(self, from_vertex, to_vertex, weight=1.0):
-        """add an edge but it is directed"""
-        self.edge_weights[(from_vertex, to_vertex)] = weight
+        self.edge_weights[(from_vertex_label, to_vertex_label)] = weight
         self.adjacency_list[from_vertex].append(to_vertex)
+        return self
 
-    def add_undirected_edge(self, vertex_a, vertex_b, weight=1.0):
+    def add_directed_edge(self, from_vertex_label, to_vertex_label, weight=1.0):
+        """add an edge but it is directed"""
+
+        from_vertex = self.get_vertex(from_vertex_label)
+        to_vertex = self.get_vertex(to_vertex_label)
+
+        self.edge_weights[(from_vertex_label, to_vertex_label)] = weight
+        self.adjacency_list[from_vertex].append(to_vertex)
+        return self
+
+    def add_undirected_edge(self, vertex_a_label, vertex_b_label, weight=1.0):
         """add an edge that is undirected"""
+        vertex_a = self.get_vertex(vertex_a_label)
+        vertex_b = self.get_vertex(vertex_b_label)
+
         self.add_directed_edge(vertex_a, vertex_b, weight)
         self.add_directed_edge(vertex_b, vertex_a, weight)
+        return self
 
-    def get_weight(self, from_vertex, to_vertex):
+    def get_weight(self, from_vertex_label, to_vertex_label):
         """return the weight for an edge"""
-        # -----------add checks for  validity here-------
+        print("----------GETTING WEIGHT-----------", from_vertex_label, to_vertex_label)
 
-        return self.edge_weights[(from_vertex, to_vertex)]
+        if not isinstance(from_vertex_label, str):
+            raise ValueError("Label is not a string")
+        if not isinstance(to_vertex_label, str):
+            raise ValueError("Label is not a string")
 
+        from_vertex = self.get_vertex(from_vertex_label)
+        to_vertex = self.get_vertex(to_vertex_label)
+        if from_vertex is None or to_vertex is None:
+            raise ValueError("This vertex is not in the graph")
 
-    def breadth_first_search(graph, start_vertex):
+        if self.edge_does_not_exist(from_vertex_label, to_vertex_label):
+            print("edge doesn't  exist")
+            return math.inf
+
+        return float(self.edge_weights[(from_vertex_label, to_vertex_label)])
+
+    def bfs(self, start_vertex_label):
         """Breadth-first search function"""
+        start_vertex = self.get_vertex(start_vertex_label)
+
+        if not isinstance(start_vertex, Vertex):
+            raise ValueError("Label is not a Vertex")
+
         distances = dict()
         discovered_set = set()
         frontier_queue = Queue()
@@ -69,8 +136,8 @@ class Graph:
 
         while frontier_queue.list.head is not None:
             current_vertex = frontier_queue.dequeue()
-            visited_list.append(current_vertex)
-            for adjacent_vertex in graph.adjacency_list[current_vertex]:
+            visited_list.append(current_vertex.label)
+            for adjacent_vertex in self.adjacency_list[current_vertex]:
                 if adjacent_vertex not in discovered_set:
                     frontier_queue.enqueue(adjacent_vertex)
                     discovered_set.add(adjacent_vertex)
@@ -80,18 +147,24 @@ class Graph:
 
         return visited_list
 
-
-    def depth_first_search(graph, start_vertex, visit_function):
+    def dfs(self, start_vertex_label):
         """ Depth-first search function """
+        start_vertex = self.get_vertex(start_vertex_label)
+
+        if not isinstance(start_vertex, Vertex):
+            raise ValueError("Label is not a Vertex")
+
+        the_DFS_list = []
+
         vertex_stack = [start_vertex]
         visited_set = set()
 
         while len(vertex_stack) > 0:
             current_vertex = vertex_stack.pop()
             if current_vertex not in visited_set:
-                visit_function(current_vertex)
+                the_DFS_list.append(current_vertex.label)
                 visited_set.add(current_vertex)
-                for adjacent_vertex in graph.adjacency_list[current_vertex]:
+                for adjacent_vertex in self.adjacency_list[current_vertex]:
                     vertex_stack.append(adjacent_vertex)
 
-
+        return the_DFS_list
